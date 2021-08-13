3e14d1e8aa356a9fe368bcaa0084ea35e3919286
Thilakshan Baskaran <telakshanb@gmail.com>
2021-08-12 21:09:25 -0400

    add order history component skeleton

---
+++ src/Components/OrderHistory/OrderHistory.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OrderHistory: React.FC = () => {
+  return (
+    <div className="orderhistory">
+      <h1>THANK YOU FOR YOUR ORDER</h1>
+    </div>
+  );
+};
+
+export default OrderHistory;
