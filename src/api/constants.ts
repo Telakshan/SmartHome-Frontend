23f5aac4500e8231e1e0c32accc9a655bb82e38d
Thilakshan Baskaran <BASKTHI@chrobinson.com>
2021-11-22 12:50:49 -0500

    new aws url

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,6 +1,6 @@
-const baseUrl = "https://powerful-ridge-26829.herokuapp.com/api/";
+//const baseUrl = "https://powerful-ridge-26829.herokuapp.com/api/";
 //const baseUrl = "http://localhost:8080/api/"
-//const baseUrl = "http://159.65.36.178:5000/api/"
+const baseUrl = "http://smarthome-env.eba-3ye7nvmm.us-east-2.elasticbeanstalk.com/api"
 
 export const productUrl = `${baseUrl}products`;
 export const stateUrl = `${baseUrl}states`;
